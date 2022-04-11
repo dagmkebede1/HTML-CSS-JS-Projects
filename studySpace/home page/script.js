3d0366dfc018c3aad7ff0e0678fc3a4125639a88
samson meseret <samsonmeseretb@gmail.com>
2022-04-11 09:31:11 -0700

    active page updated

---
+++ studySpace/home page/script.js
@@ -0,0 +1,8 @@
+// const activePage = window.location.pathname;
+// const navLiinks = document
+//   .querySelectorAll(".side-container a")
+//   .forEach((link) => {
+//     if (link.href.includes(`${activePage}`)) {
+//       link.classList.add("active");
+//     }
+//   });
